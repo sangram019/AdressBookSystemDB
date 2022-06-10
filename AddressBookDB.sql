a4a1a8f57176df5eac280fd90d4aa170a1c141bb
Sangram <sangram15.behera@gmail.com>
2022-06-10 11:08:44 +0530

    Added By Sangram

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -20,6 +20,13 @@ PhoneNumber VARCHAR(10),
 Email VARCHAR(100)
 );
 
+UC3 : Insert new contacts to AddressBook
 
 
+INSERT INTO AddressBook VALUES ('sangram','behera','bbsr','bbsr','odisha',560037,'7077445649','sangram@gmail.com'),
+('rakesh','behera','bdk','bdk','odisha',756001,'9999345743' ,'rakesh@gmail.com'),
+('dinesh','behera','bdk','bdk','odisha',756137,'9876543383','dinesh@gmail.com'),
+('prajna','behera','bdk','bdk','odisha',756118,'9875356787','prajna@gmail.com'),
+('susant','behera','bdk','bdk','odisha',750112,'8765567754','susant@gmail.com');
 
+SELECT * FROM AddressBook;
